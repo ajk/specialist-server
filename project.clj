369d383280e27e0f9a09bc7d9e482dd446e0be75
Antti Koskinen <4ntti.koskinen@gmail.com>
2017-10-28 11:49:48 +0300

    Use Clojure 1.9.0-beta3

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/ajk/specialist-server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta3"]
                  [clj-antlr "0.2.4"]]
 
   :min-lein-version "2.7.1"
