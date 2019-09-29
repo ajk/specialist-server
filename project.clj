4433f3cd2790dd0adca221a0fd04f9f4d67a2525
Antti Koskinen <4ntti.koskinen@gmail.com>
2019-09-29 11:24:33 +0300

    Updated deps

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject ajk/specialist-server "0.6.0"
+(defproject ajk/specialist-server "0.7.0-SNAPSHOT"
   :description "Spec-driven Clojure GraphQL server"
   :url "https://github.com/ajk/specialist-server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.logging "0.4.1"]
-                 [clj-antlr "0.2.4"]]
+                 [org.clojure/tools.logging "0.5.0"]
+                 [clj-antlr "0.2.5"]]
 
-  :min-lein-version "2.7.1"
+  :min-lein-version "2.9.1"
   :pedantic :abort)
